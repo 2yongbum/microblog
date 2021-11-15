e8e427b8f060f28bc26c9459f0e8e563098a7d70
Yongbum Lee <2yongbum@gmail.com>
2021-11-15 15:53:41 +0900

    Chapter 13: I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
+from flask_babel import lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,9 +19,16 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
